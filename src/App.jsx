492ee555e184a7bdffbefb3a89c6bacf4eeafda1
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-03-26 10:07:02 +0700

    (+) search github user

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import BgImage from "./assets/hero-image-github-profile.png";
+import SearchICon from "./assets/Search.svg";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const onChange = async (e) => {
+    const username = e.target.value ?? "";
+
+    if (!username) {
+      setData({});
+
+      return;
+    }
+
+    const res = await axios(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: `token ${process.env.PERSONAL_TOKEN}`,
+      },
+    }).catch(() => {
+      setData({});
+
+      return;
+    });
+    const data = res?.data ?? {};
+
+    setData(data);
+  };
+
+  const isEmptyObject = (obj) => {
+    for (const prop in obj) {
+      if (Object.hasOwn(obj, prop)) {
+        return false;
+      }
+    }
+
+    return true;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen min-w-screen">
+      <div className="relative flex items-center justify-center gap-2 p-12 overflow-hidden">
+        <img
+          src={BgImage}
+          alt="background-image"
+          className="absolute top-0 left-0 object-fit"
+        />
 
-export default App
+        <div className="w-1/3 space-y-4 z-10">
+          <div className="w-full p-4 flex items-center gap-3 bg-[#20293A] rounded-lg border border-transparent focus-within:border-[#3662E3]">
+            <img src={SearchICon} alt="search-icon" />
+            <input
+              type="text"
+              placeholder="username"
+              className="outline-none border-none bg-transparent text-white placeholder:text-[#4A5567]"
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+
+          <div
+            className={`${
+              isEmptyObject(data) ? "invisible" : "visible"
+            } w-full h-full p-2 flex items-center gap-3 rounded-lg bg-[#20293A]`}
+          >
+            <img
+              src={data?.avatar_url}
+              alt="profile-image"
+              className="size-20 object-cover rounded-lg"
+            />
+            <div className="flex flex-col justify-between gap-2">
+              <p className="font-semibold text-white">{data?.name}</p>
+              <p className="text-sm text-[#4A5567]">{data?.bio ?? "No Bio"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ export default {
   theme: {
     extend: {},
   },
+  variants: {
+    borderColor: ["focus-within"],
+  },
   plugins: [],
 };
